58886f9c2ec58066a7c7a0ab6627181df0168020
Seebles <seebles72@gmail.com>
2024-01-17 14:36:33 -0700

    e

--- Node2D.gd
+++ Node2D.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 
 @onready var lilguy = $Guy
 @onready var smashshield = $smashshield
-@export var facing := 1 #right
-@export var speed = 300
+@export var facing := 1 #rig
+const SPEED = 300
+const JUMP_VELOCITY = -400.0
 @export var attacking = false
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func get_input():
 	var input_direction: float = Input.get_axis("ui_left", "ui_right")
-	velocity.x = input_direction * speed
+	velocity.x = input_direction * SPEED
 
 
 func move():
@@ -52,10 +56,24 @@ func attack():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	move()
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if not attacking:
 		get_input()
 	if not smashshield.draw():
